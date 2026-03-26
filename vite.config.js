596cdb27adcb7127bae73b872e0935e6ab647acc
rutujatelore <rutujatelore100@gmail.com>
2026-03-27 02:45:39 +0530

    fix host blocking issue

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: "0.0.0.0",
-    port: 5173
+    port: 5173,
+    allowedHosts: true
   },
   preview: {
     host: "0.0.0.0",
-    port: 10000
+    port: 10000,
+    allowedHosts: true
   }
 })
